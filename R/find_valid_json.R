9a3389ad7c4988a71adfb727201e58e1b7d67825
HedvigS <hedvig.skirgard@gmail.com>
2023-09-01 21:23:06 +0200

    Create find_valid_json.R

---
+++ R/find_valid_json.R
@@ -0,0 +1,25 @@
+#' Helper function to resolve the path (e.g. directory or md.json file)
+#'
+#' @param path the path to resolve
+#' @export
+#' @return A string containing the path to the metadata.json file
+find_valid_path_json <- function(path) {
+    path <- base::normalizePath(path, mustWork = FALSE)
+    # no file
+    if (!file.exists(path)) {
+        stop(sprintf("Path %s does not exist", path))
+        # given a metadata.json file
+    } else if (!dir.exists(path) && tolower(tools::file_ext(path)) == 'json') {
+        return(load_metadata(path))
+        # given a dirname, try find the metadata file.
+    } else if (dir.exists(path)) {
+        mdfiles <- list.files(path, "*metadata.json", full.names = TRUE, recursive=TRUE)
+        # limit to 10 so we don't risk loading all json files on the computer
+        for (m in head(mdfiles, 10)) {
+            try({load_metadata(m)
+                return(m)}, silent = TRUE)
+        }
+        stop(sprintf("no metadata JSON file found in %s", path))
+    }
+    stop("Need either a metadata.json file or a directory with metadata.json")
+}
